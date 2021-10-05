b13071adcbabae8e386103cc800518b2f5565cad
Mohamed Arafaath <mohamed_arafaath@outlook.com>
2021-10-05 20:16:02 +0530

    Robotics Programming (AIRO)
    Robot Programming 1st Year 1st semesters programs

--- source/rp_02_cpp_intro/01_references.cpp
+++ source/rp_02_cpp_intro/01_references.cpp
@@ -2,11 +2,12 @@
 using namespace std; // we will discover this later
 
 int main(int argc, char** argv) {
-  // declare an inr
+  // declare an int
   int a = 5;
   cerr << "A is: " << a << endl;
 
   // declare a reference to an int, and assign it to a
+  // int &ref = a;
   // a reference is like an alias to a variable
   // references need always to be initialized on creation
   // think as a reference as a dereferenced pointer
